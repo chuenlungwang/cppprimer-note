d6dae53a0d1345a9e082874c7a0ce3df61fccb1d
chuenlungwang <chuenlungwang@gmail.com>
2018-07-31 12:13:27 +0800

    :memo:CH7~Const object may have mutable part.

--- code/const_struct.cc
+++ code/const_struct.cc
@@ -18,5 +18,13 @@ int main()
     //pd->d = 6.6;
     *pd->pi = 100;
     std::cout << "i: " << d.i << " d: " << d.d << " pi: " << *d.pi << std::endl;
+
+    //需要强调的一点是：const 对象中依然有某些可变的部分，原因在虽然成员本身是
+    //const 的，但成员可能指向是非 const 对象，
+    //抑或成员的常量成员函数返回的值并不是 const 的
+    const struct Data cnst_d = {1, 2.2, new int{20}};
+    std::cout << *cnst_d.pi << std::endl;
+    *cnst_d.pi = 999;
+    std::cout << *cnst_d.pi << std::endl;
     return 0;
 }
