aa472fe1e787c8af2b985e7eae2e28b14b3d79d1
HammerWang <chuenlungwang@qq.com>
2018-08-15 00:40:28 +0800

    :sparkles:CH15~Derived Class Declaration

--- code/Quote.cc
+++ code/Quote.cc
@@ -17,7 +17,12 @@ protected:
 
 class Bulk_quote : public Quote {
 public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string &, double, std::size_t, double);
     double net_price(std::size_t) const override;
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
 };
 
 double
