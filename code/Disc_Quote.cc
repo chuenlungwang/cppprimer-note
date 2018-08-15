05fd99c65b8d77c3ce29a8dd34d8280a769b1446
HammerWang <chuenlungwang@qq.com>
2018-08-16 00:04:57 +0800

    :sparkles:CH15~Pure Virtual Function

---
+++ code/Disc_Quote.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Quote {
+public:
+    Quote() = default;
+    Quote(const std::string &book, double sales_price):
+        bookNo(book), price(sales_price) {  }
+    std::string isbn() const { return bookNo; }
+    virtual double net_price(std::size_t n) const { return n * price; }
+    virtual ~Quote() = default;
+private:
+    std::string bookNo;
+protected:
+    double price = 0.0;
+};
+
+class Disc_quote : public Quote {
+public:
+    Disc_quote() = default;
+    Disc_quote(const std::string &book, double price, std::size_t qty, double disc)
+        : Quote(book, price), quantity(qty), discount(disc) { }
+    virtual double net_price(std::size_t n) const override = 0;
+protected:
+    std::size_t quantity = 0;
+    double discount = 0.0;
+};
+
+double
+print_total(std::ostream &os, const Quote &item, std::size_t n)
+{
+    double ret = item.net_price(n);
+    os << "ISBN: " << item.isbn()
+       << " # sold: " << n << " total due: " << ret << std::endl;
+    return ret;
+}
