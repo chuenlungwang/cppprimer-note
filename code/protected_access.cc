7f5240e9b6ac094aba7c7cce6fbb455d331e5129
bitwang <chuenlungwang@gmail.com>
2018-08-22 23:30:45 +0800

    :memo:CH15~Finish OOP

--- code/protected_access.cc
+++ code/protected_access.cc
@@ -72,7 +72,7 @@ class Pal {
 public:
     int f(Base b) { return b.prot_mem; }
     //int f2(Sneaky s) { return s.j; }
-    int f3(Sneaky s) { return s.prot_mem; }
+    int f3(Sneaky s) { return s.priv_mem; }
 };
 
 class D2 : public Pal {
