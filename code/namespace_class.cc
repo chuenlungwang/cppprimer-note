e32eab42266c849cfb22705f94541d7498a8bcc4
chuenlungwang <chuenlungwang@gmail.com>
2018-09-22 19:26:09 +0800

    Add namespace with class

--- code/namespace_class.cc
+++ code/namespace_class.cc
@@ -1,3 +1,5 @@
+#include <string>
+
 namespace A {
     int i;
     int k;
@@ -17,3 +19,60 @@ namespace A {
 }
 
 int A::C1::f3() { return h; }
+
+namespace NS {
+    class Quote {  };
+    void display(const Quote &) {  }
+}
+
+class Bulk_item : public NS::Quote {  };
+
+////////////////////////////////////////////////
+
+namespace NS {
+    print(int) {}
+    print(double) {}
+}
+
+/////////////////////////////////////////////////
+
+namespace libs_R_us {
+    void print(int) {}
+    void print(double) {}
+}
+
+void print(const std::string &) {}
+void print(int) {}
+using namespace libs_R_us;
+
+void fooBar(int ival)
+{
+    print("Values: ");
+    libs_R_us::print(ival);
+    ::print(ival);
+    print(1234.4567);
+}
+
+////////////////////////////////////////////////
+
+namespace AW {
+    int print(int) { return 1; }
+}
+
+namespace Primer {
+    double print(double) { return 1; }
+}
+
+long double print(long double) { return 1; }
+
+////////////////////////////////////////////////
+
+int main() {
+    Bulk_item book1;
+
+    //Argument-Dependent lookup
+    display(book1);
+
+    using namespace AW;
+    using namespace Primer;
+}
